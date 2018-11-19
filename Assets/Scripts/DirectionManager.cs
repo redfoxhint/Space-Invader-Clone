c924e348979496879e01e6d73cb88eb80619cda4
Adrian De Andrade <redfoxhint4@hotmail.com>
2018-11-19 01:09:09 -0500

    Initial commit

---
+++ Assets/Scripts/DirectionManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionManager : MonoBehaviour
+{
+    public enum Direction { LEFT, RIGHT, FORWARD, BACKWARD };
+    public Direction currentDirection;
+
+    [SerializeField] private Transform cameraPivot;
+    [SerializeField] private Transform player;
+
+    [SerializeField] Quaternion currentRotation;
+
+    private void Start()
+    {
+        currentDirection = Direction.FORWARD;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            currentDirection = Direction.FORWARD;
+            currentRotation = CalculateRotation(Vector3.zero);
+            ChangeDirection();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            currentDirection = Direction.RIGHT;
+            currentRotation = CalculateRotation(new Vector3(0f, 90f, 0f));
+            ChangeDirection();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            currentDirection = Direction.LEFT;
+            currentRotation = CalculateRotation(new Vector3(0f, -90f, 0f));
+            ChangeDirection();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            currentDirection = Direction.BACKWARD;
+            currentRotation = CalculateRotation(new Vector3(0f, 180f, 0f));
+            ChangeDirection();
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        switch (currentDirection)
+        {
+            case Direction.LEFT:
+                cameraPivot.rotation = currentRotation;
+                player.rotation = currentRotation;
+                break;
+            case Direction.RIGHT:
+                cameraPivot.rotation = currentRotation;
+                player.rotation = currentRotation;
+                break;
+            case Direction.FORWARD:
+                cameraPivot.rotation = currentRotation;
+                player.rotation = currentRotation;
+                break;
+            case Direction.BACKWARD:
+                cameraPivot.rotation = currentRotation;
+                player.rotation = currentRotation;
+                break;
+        }
+    }
+
+    private Quaternion CalculateRotation(Vector3 rotationToConvert)
+    {
+        Quaternion newRotation = Quaternion.Euler(rotationToConvert);
+        return newRotation;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float followTime;
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private DirectionManager dirManager;
+
+    private Vector3 movement;
+    private Vector3 input;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        PlayerInput(dirManager.currentDirection);
+    }
+
+    private void PlayerInput(DirectionManager.Direction dir)
+    {
+        switch(dir)
+        {
+            case DirectionManager.Direction.FORWARD:
+                input = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+                movement = input * moveSpeed * Time.deltaTime;
+                transform.position += movement;
+                SetCameraPosition(dir);
+                break;
+
+            case DirectionManager.Direction.BACKWARD:
+                input = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+                movement = input * moveSpeed * Time.deltaTime;
+                transform.position -= movement;
+                SetCameraPosition(dir);
+                break;
+
+            case DirectionManager.Direction.LEFT:
+                input = new Vector3(0f, 0f, Input.GetAxis("Horizontal"));
+                movement = input * moveSpeed * Time.deltaTime;
+                transform.position += movement;
+                SetCameraPosition(dir);
+                break;
+
+            case DirectionManager.Direction.RIGHT:
+                input = new Vector3(0f, 0f, Input.GetAxis("Horizontal"));
+                movement = input * moveSpeed * Time.deltaTime;
+                transform.position -= movement;
+                SetCameraPosition(dir);
+                break;
+        }
+    }
+
+    private void SetCameraPosition(DirectionManager.Direction dir)
+    {
+
+    }
+}
