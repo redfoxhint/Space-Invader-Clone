844e9c4a799e25f658883eac5e143edafe65463d
Adrian De Andrade <redfoxhint4@hotmail.com>
2018-11-21 15:17:57 -0500

    stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,36 +27,52 @@ public class PlayerController : MonoBehaviour
     {
         if(!DirectionManager.instance.isChangingDirection)
         {
-            PlayerInput(DirectionManager.instance.currentDirection);
+            InputAndConstraints();
         }
     }
 
-    private void PlayerInput(DirectionManager.Direction dir)
+    private void InputAndConstraints()
     {
-        switch (dir)
+        switch (DirectionManager.instance.currentDirection)
         {
             case DirectionManager.Direction.FORWARD:
                 input = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
                 movement = input * moveSpeed * Time.deltaTime;
                 transform.position += movement;
+
+                if(transform.position.x > 5.6f) transform.position = new Vector3(5.6f, 0f, 0f);
+                if (transform.position.x < -5.6f) transform.position = new Vector3(-5.6f, 0f, 0f);
+
                 break;
 
             case DirectionManager.Direction.BACKWARD:
                 input = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
                 movement = input * moveSpeed * Time.deltaTime;
                 transform.position -= movement;
+
+                if (transform.position.x > 5.6f) transform.position = new Vector3(5.6f, 0f, 0f);
+                if (transform.position.x < -5.6f) transform.position = new Vector3(-5.6f, 0f, 0f);
+
                 break;
 
             case DirectionManager.Direction.LEFT:
                 input = new Vector3(0f, 0f, Input.GetAxis("Horizontal"));
                 movement = input * moveSpeed * Time.deltaTime;
                 transform.position += movement;
+
+                if (transform.position.z > 5.6f) transform.position = new Vector3(0f, 0f, 5.6f);
+                if (transform.position.z < -5.6f) transform.position = new Vector3(0f, 0f, -5.6f);
+
                 break;
 
             case DirectionManager.Direction.RIGHT:
                 input = new Vector3(0f, 0f, Input.GetAxis("Horizontal"));
                 movement = input * moveSpeed * Time.deltaTime;
                 transform.position -= movement;
+
+                if (transform.position.z > 5.6f) transform.position = new Vector3(0f, 0f, 5.6f);
+                if (transform.position.z < -5.6f) transform.position = new Vector3(0f, 0f, -5.6f);
+
                 break;
         }
     }
