722c7f56ac1a884fd0752175ae3fad65f0f20d81
Adrian De Andrade <redfoxhint4@hotmail.com>
2018-11-27 12:19:22 -0500

    Added shooting (kind of)

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed;
+
+    private void Update()
+    {
+        transform.Translate(transform.forward * bulletSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private Transform shootPoint;
+
+    [SerializeField] private GameObject projectilePrefab;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(projectilePrefab, shootPoint.position, Quaternion.identity);
+            Destroy(bullet, 2.5f);
+        }
+    }
+}
